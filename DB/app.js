ab1788f077622b8ec79e7f45b48a32b4d0764686
John Entrieri <john.entrieri@gmail.com>
2019-03-31 12:07:18 -0400

    Basic API calls for characters

--- DB/app.js
+++ DB/app.js
@@ -1,7 +1,14 @@
+//External Modules
 var express = require('express');
-var private = require('./private/private')
 var mongoose = require('mongoose');
+
+//Server-specific private info
+var private = require('./private/private')
+
+//MongoDB Schemas
 var Character = require('./models/characterModel');
+
+//API Route
 var apiRouter = require('./routes/api');
 
 var app = express();
@@ -9,6 +16,7 @@ var app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+//MongoDB Connection
 mongoose.Promise = global.Promise;
 mongoose.connect(private.dbURL, { useNewUrlParser: true }); 
 
--- DB/routes/api.js
+++ DB/routes/api.js
@@ -4,6 +4,12 @@ var router = express.Router();
 var character = require('../controllers/characterController')
 
 router.route('/characters/')
-  .get(character.getAllCharacters);
+  .get(character.getAllCharacters)
+  .post(character.createCharacter);
+
+router.route('/characters/:charId')
+  .get(character.getCharacter)
+  .put(character.updateCharacter)
+  .delete(character.deleteCharacter);
 
 module.exports = router;
